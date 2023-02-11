e407ab201d190292d90f4eda57f4eec215105ee1
Aric Maddux <madducks@gmail.com>
2023-02-11 16:46:30 +0000

    added tarotHandler file

---
+++ tarotHandler.py
@@ -0,0 +1,31 @@
+from random import shuffle
+
+from tarotdeck import TarotDeck
+
+class TarotHandler:
+    def __init__(self):
+        self.max_cards = 3
+        self.decks = ('jodocamoin', 'riderwaitesmith', 'crowleythoth')
+
+    def draw(self, num=1, deck_type='jodocamoin'):
+        response_text = []
+        images = []
+        if deck_type not in self.decks:
+            response_text.append("Deck type must be one of {}".format(self.decks))
+        elif num > self.max_cards:
+            response_text.append("I will draw a max of {} cards.".format(self.max_cards))
+        else:
+            deck = TarotDeck(deck_type)
+            shuffle(deck)
+            num_cards = num
+            for c in range(num_cards):
+                card = deck.pop()
+                if 'majors' not in card.image:
+                    response_text.append(card.rank + ' of ' + card.kind)
+                else:
+                    response_text.append(card.rank + ': ' + card.kind)
+                images.append(card.image)
+        return response_text, images
+
+
+
